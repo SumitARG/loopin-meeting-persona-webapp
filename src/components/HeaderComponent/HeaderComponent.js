173cc50f3c80dfe8737f6717d464ca43fc0d41e6
Sumit Gokhale <sumitgokhale@Sumits-MacBook-Air.local>
2023-06-26 23:05:44 +0530

    Header Created

---
+++ src/components/HeaderComponent/HeaderComponent.js
@@ -0,0 +1,14 @@
+import "./HeaderComponent.scss";
+import COMPANY_LOGO from "../../assets/images/Vector.svg"
+import COMPANY_NAME from "../../assets/images/Group.svg"
+
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+        <img src={COMPANY_LOGO} alt="Logo" />
+        <img src={COMPANY_NAME} alt="Name" />
+    </div>
+  )
+}
+
+export default HeaderComponent
