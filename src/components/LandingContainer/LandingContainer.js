4e8a2f223484e901e6e379faff982f470d89f323
Sumit Gokhale <sumitgokhale@Sumits-MacBook-Air.local>
2023-07-04 23:31:27 +0530

    Persona List View Completed

--- src/components/LandingContainer/LandingContainer.js
+++ src/components/LandingContainer/LandingContainer.js
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import { Outlet } from "react-router";
+import PersonaList from "../CommonComponents/PersonList/PersonaList";
 import LandingPageComponent from "../LandingPageComponent/LandingPageComponent";
 import "./LandingContainer.scss";
 // import { Outlet } from "react-router-dom";
 
 const LandingContainer = () => {
+
+  const [showPersona, setShowPersona] = useState(false);
+
+  const showButtonHandler = () => {
+    let tempShowPersona = showPersona;
+    setShowPersona(!tempShowPersona);
+  }
+
   return (
     <div>
-      <LandingPageComponent />
-      <Outlet/>
+      <LandingPageComponent showPersona={showButtonHandler}/>
+      <Outlet />
+      <div className="personas-container">
+      {showPersona && <PersonaList closePersonaList={showButtonHandler}/>}
+      </div>
     </div>
   );
 };
