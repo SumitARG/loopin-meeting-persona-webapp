42e931845d58710e790b4363f6f148865aae9367
Sumit Gokhale <sumitgokhale@Sumits-MacBook-Air.local>
2023-07-11 18:27:33 +0530

    Lotties added and handled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import { useEffect } from "react";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import "./App.scss";
 import FooterComponent from "./components/FooterComponent/FooterComponent";
 import HeaderComponent from "./components/HeaderComponent/HeaderComponent";
-import LandingPageComponent from "./components/LandingPageComponent/LandingPageComponent";
 // import QuizContainer from "./components/QuizContainer/QuizContainer";
 import { LOCAL_STORAGE, ROUTE_NAMES } from "./config/Constants";
 
@@ -16,10 +15,8 @@ function App() {
       LOCAL_STORAGE.ANSWERS_STORAGE,
       JSON.stringify({ 1: "", 2: "", 3: "", 4: "", 5: "", 6: "" })
     );
-    // if (location.pathname === "/") {
       navigate(`/${ROUTE_NAMES.HOME_ROUTE}`);
-    // }
-  }, []);
+  }, []);// eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <div className="App" id="mainApp">
